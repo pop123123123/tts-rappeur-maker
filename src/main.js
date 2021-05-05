0bf6ce1a41206cb70a8fb3e9b7a224940e0431b6
Louis MOUHAT <louis.mouhat@gmail.com>
2021-05-05 03:00:19 +0200

    import vue-material components one by one

--- src/main.js
+++ src/main.js
@@ -1,10 +1,31 @@
 import Vue from 'vue';
-import VueMaterial from 'vue-material';
+import { MdApp, MdAppContent, MdAppToolbar } from 'vue-material/dist/components/MdApp';
+import MdButton from 'vue-material/dist/components/MdButton';
+import {
+  MdCard, MdCardActions, MdCardContent, MdCardHeader,
+} from 'vue-material/dist/components/MdCard';
+import MdDivider from 'vue-material/dist/components/MdDivider';
+import { MdField, MdInput, MdTextarea } from 'vue-material/dist/components/MdField';
+import MdSnackbar from 'vue-material/dist/components/MdSnackbar';
+import MdSwitch from 'vue-material/dist/components/MdSwitch';
 import App from './App.vue';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
 
-Vue.use(VueMaterial);
+Vue.use(MdApp);
+Vue.use(MdAppContent);
+Vue.use(MdAppToolbar);
+Vue.use(MdButton);
+Vue.use(MdCard);
+Vue.use(MdCardActions);
+Vue.use(MdCardContent);
+Vue.use(MdCardHeader);
+Vue.use(MdDivider);
+Vue.use(MdField);
+Vue.use(MdInput);
+Vue.use(MdSnackbar);
+Vue.use(MdSwitch);
+Vue.use(MdTextarea);
 
 Vue.material = {
   ...Vue.material,
